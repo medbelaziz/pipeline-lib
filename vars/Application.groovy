26f139c0fe824b1c04399165708edbd88526e368
Mohammed Belaziz/c/Mohammed <medbelaziz@gmail.com>
2019-12-12 12:16:15 +0100

    update

--- vars/Application.groovy
+++ vars/Application.groovy
@@ -1,8 +1,44 @@
-package com.orange
+#!/usr/bin/groovy
 
-class Application {
-	static void main(args) {
-		
-		println("Hello ..");
+def call() {
+	
+	pipeline {
+		agent any
+		stages {
+			stage('Build') {
+				steps { echo 'Building..................' }
+			}
+
+			stage('Deploy') {
+				parallel {
+					stage('Deploy') {
+						steps { echo 'Deploying......................' }
+					}
+
+					stage('Deploy2') {
+						steps { echo 'From depoying 2..............' }
+					}
+
+					stage('Deploy3') {
+						steps { echo 'Deploy 3..................' }
+					}
+				}
+			}
+
+			stage('Test') {
+				steps { echo 'Testing .................' }
+			}
+		}
+		post {
+			always { echo 'One way or another, I have finished' }
+
+			success { echo 'I succeeeded!' }
+
+			unstable { echo 'I am unstable :/' }
+
+			failure { echo 'I failed :(' }
+
+			changed { echo 'Things were different before...' }
+		}
 	}
 }
