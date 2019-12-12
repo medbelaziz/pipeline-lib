fc51fb5bc037cadc552733e8c02369c9c7de1593
Mohammed Belaziz/c/Mohammed <medbelaziz@gmail.com>
2019-12-12 13:29:49 +0100

    update

--- vars/Application.groovy
+++ vars/Application.groovy
@@ -2,43 +2,5 @@
 
 def call() {
 
-	pipeline {
-		agent any
-		stages {
-			stage('Checkout') {
-				steps {
-					echo 'Checkouting..................'
-					GitCheckout(
-							branch: "jenkins_conf",
-							url: "https://github.com/medbelaziz/bookService.git"
-							)
-				}
-			}
-
-			stage('Build') { steps { echo 'Building..................' } }
-
-			stage('Deploy') {
-				parallel {
-					stage('Deploy') { steps { echo 'Deploying......................' } }
-
-					stage('Deploy2') { steps { echo 'From depoying 2..............' } }
-
-					stage('Deploy3') { steps { echo 'Deploy 3..................' } }
-				}
-			}
-
-			stage('Test') { steps { echo 'Testing .................' } }
-		}
-		post {
-			always { echo 'One way or another, I have finished' }
-
-			success { echo 'I succeeeded!' }
-
-			unstable { echo 'I am unstable :/' }
-
-			failure { echo 'I failed :(' }
-
-			changed { echo 'Things were different before...' }
-		}
-	}
+	
 }
